f934e152ebdda2191883d0d6947fb248d5e2515b
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-10-30 20:58:54 +0800

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 function Footer(){
     return(
-        <p>Created by <a href="https://github.com/Shishwami">Shishwami</a></p>
+        <p id="footer"> by <a href="https://github.com/Shishwami">Shishwami</a></p>
     );
 }
 
