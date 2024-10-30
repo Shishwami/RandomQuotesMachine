cb8eda35a449975eb110842c4e057150a9aa7e2b
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-10-30 20:43:50 +0800

    Create Socials.js

---
+++ src/Components/Socials.js
@@ -0,0 +1,9 @@
+function Socials({quote,author}) {
+    return (
+        <div className="social-container">
+            <a href=""><i class="fa fa-twitter"></i></a>
+            <a href=""><i class="fa fa-tumblr"></i></a>
+        </div>
+    );
+}
+export default Socials;
