a87845779d5896e43e3549e1754c2a85095b02c8
Shishwami <157940784+Shishwami@users.noreply.github.com>
2024-10-30 21:33:03 +0800

    Update Socials.js

--- src/Components/Socials.js
+++ src/Components/Socials.js
@@ -1,9 +1,28 @@
-function Socials({quote,author}) {
+function Socials({ quote, author }) {
+    const encodedQuote = encodeURIComponent(quote);
+    const encodedAuthor = encodeURIComponent(author);
     return (
         <div className="social-container">
-            <a href=""><i class="fa fa-twitter"></i></a>
-            <a href=""><i class="fa fa-tumblr"></i></a>
+            <a 
+                href={`https://twitter.com/intent/tweet?text="${encodedQuote}" - ${encodedAuthor}&url=https://shishwami.github.io/RandomQuotesMachine/`} 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                id="tweet-quote"
+            >
+                <i className="fa fa-twitter"></i>
+            </a>
+            <a 
+                className="button" 
+                id="tumblr-quote" 
+                title="Post this quote on Tumblr!" 
+                target="_blank" 
+                href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=${encodedAuthor}&content=${encodedQuote}&canonicalUrl=https://shishwami.github.io/RandomQuotesMachine/&shareSource=tumblr_share_button`} 
+               
+            >
+                <i className="fa fa-tumblr"></i>
+            </a>
         </div>
     );
 }
+
 export default Socials;
